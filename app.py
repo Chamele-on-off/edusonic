8b5955079d69e9425246a1370ef3d917c7302f97
Chamele-on-off <mot124@yandex.ru>
2025-08-13 18:36:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, send_from_directory
 import os
 
 app = Flask(__name__)
+
+# Разрешаем все возможные расширения изображений
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif', 'webp'}
 
 @app.route('/')
 def animation_test():
     return render_template('teacher.html')
 
-@app.route('/api/avatar_frames')
-def get_avatar_frames():
-    frames = {
-        'mouth_neutral': [],
-        'blink': [],
-        'mouth_aa': [],
-        'mouth_bb': [],
-        # Добавьте остальные группы по аналогии
-    }
-    
-    frames_dir = os.path.join('static', 'avatar', 'frames')
-    for filename in os.listdir(frames_dir):
-        if filename.startswith('mouth_neutral_'):
-            frames['mouth_neutral'].append(f'/static/avatar/frames/{filename}')
-        elif filename.startswith('blink_'):
-            frames['blink'].append(f'/static/avatar/frames/{filename}')
-        elif filename.startswith('mouth_aa_'):
-            frames['mouth_aa'].append(f'/static/avatar/frames/{filename}')
-        elif filename.startswith('mouth_bb_'):
-            frames['mouth_bb'].append(f'/static/avatar/frames/{filename}')
-        # Добавьте обработку остальных префиксов
-    
-    return jsonify(frames)
+@app.route('/static/avatar/frames/<path:filename>')
+def serve_frames(filename):
+    return send_from_directory('static/avatar/frames', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
