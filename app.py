99891c1a019c71a7114ef0d7a0c1f7b7a1cdb397
Chamele-on-off <mot124@yandex.ru>
2025-10-08 02:28:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ socketio = SocketIO(
     app, 
     cors_allowed_origins="*", 
     async_mode='threading',
-    ping_timeout=15000,           # Увеличенный таймаут ping
+    ping_timeout=150000,           # Увеличенный таймаут ping
     ping_interval=40,          # Более частые ping
     max_http_buffer_size=1e8,  # Увеличенный размер буфера
     logger=True,               # Логирование для отладки
